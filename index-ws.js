c9cd2555b977234ed30e0786ea56cc2e3990a83e
hafid-nda <zaeinyh@gmail.com>
2022-10-26 16:53:43 +0700

    add socket.io-client

--- index-ws.js
+++ index-ws.js
@@ -5,16 +5,15 @@ const express = require('express')
 const app = express()
 const server = http.createServer(app);
 const io = new Server(server);
-
 const dotenv = require('dotenv')
 
 dotenv.config();
 const host = process.env.MQTT_HOST
-const port = process.env.MQTT_PORT
+const port = process.env.MQTT_PORT  
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
 
 const connectUrl = `mqtt://${host}:${port}`
-const client = mqtt.connect(connectUrl, {
+const client =   mqtt.connect(connectUrl, {
     clientId,
     clean: true,
     connectTimeout: 4000,
@@ -24,6 +23,11 @@ const client = mqtt.connect(connectUrl, {
 })
 
 const topic = process.env.MQTT_TOPIC
+
+//prototype chat app
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
 
 client.on('connect', () => {
     console.log('Connected')
@@ -44,20 +48,30 @@ client.on('message', (topic, payload) => {
     //listen connection from socketio client
     io.on('connection', (socket) => {
         console.log(topic, payload.toString())
-
-        //emit payload (socketio)
-
     })
-})
+    
+        //emit payload (socketio)
+        
+    })
 
-io.on('connection', (socket) => {
-    console.log('a user connected')
+    io.on('connection', (socket) => {
+        console.log('A user connected');
+        socket.on('chat message', (msg) => {
+        console.log('message from client: ' + msg);
+        io.emit('chat message', msg);
+    })
 
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('User disconnected');
     })
 });
 
-server.listen(port, () => {
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString())
+    io.emit('chat message', message.toString());  
+  })    
+
+server.listen(port, () => { 
     console.log('listening on *:' + port);
 })
