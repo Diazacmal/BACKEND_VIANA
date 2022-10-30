a626d8f1753634227fc47e92968bb51f188f4781
Yusuf Ayuba <yusufayuba@live.com>
2022-10-30 21:02:41 +0700

    update index-ws

--- index-ws.js
+++ index-ws.js
@@ -4,30 +4,49 @@ const http = require('http');
 const express = require('express')
 const app = express()
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
 const dotenv = require('dotenv')
 
 dotenv.config();
 const host = process.env.MQTT_HOST
-const port = process.env.MQTT_PORT  
+const port = process.env.MQTT_PORT
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
 
 const connectUrl = `mqtt://${host}:${port}`
-const client =   mqtt.connect(connectUrl, {
+const client = mqtt.connect(connectUrl, {
     clientId,
     clean: true,
     connectTimeout: 4000,
-    username: 'emqx',
-    password: 'public',
+    username: process.env.MQTT_USER,
+    password: process.env.MQTT_PASSWORD,
     reconnectPeriod: 1000,
 })
 
 const topic = process.env.MQTT_TOPIC
 
-//prototype chat app
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
+//set index
+app.get('/', (request, response) => {
+    response.json({ info: 'Hello world' })
+})
+
+//listen from frontend
+io.on('connection', (socket) => {
+    console.log('a user connected')
+    socket.on('ping', () => {
+        //console.log(msg)
+        io.emit('pong')
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+});
 
 client.on('connect', () => {
     console.log('Connected')
@@ -43,35 +62,15 @@ client.on('connect', () => {
 
 //listen from mqtt server
 client.on('message', (topic, payload) => {
-    console.log('Received Message:', topic, payload.toString())
+    var str = payload.toString()
+    var str2 = str.replace("b'{", "{")
+    var str3 = str2.replace("}'", "}")
 
-    //listen connection from socketio client
-    io.on('connection', (socket) => {
-        console.log(topic, payload.toString())
-    })
-    
-        //emit payload (socketio)
-        
-    })
+    console.log('Received Message:', str3)
+    //emit payload mqtt to frontend (socketio)
+    io.emit('payload', JSON.parse(str3))
+})
 
-    io.on('connection', (socket) => {
-        console.log('A user connected');
-        socket.on('chat message', (msg) => {
-        console.log('message from client: ' + msg);
-        io.emit('chat message', msg);
-    })
-
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    })
-});
-
-client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString())
-    io.emit('chat message', message.toString());  
-  })    
-
-server.listen(port, () => { 
+server.listen(port, () => {
     console.log('listening on *:' + port);
 })
